10816f88936e4cdec70d99d0c54c189f129f2e5e
Kenyang Lual <100993774+Kenyang1@users.noreply.github.com>
2024-11-05 13:07:26 -0500

    Update MenuOverlay.jsx

--- app/components/MenuOverlay.jsx
+++ app/components/MenuOverlay.jsx
@@ -1,15 +1,17 @@
-// MenuOverlay component for the portfolio website
+// Import React to create our component
 import React from 'react';
+// Import the NavLink component, which will be used to create links
 import NavLink from './NavLink';
 
+// MenuOverlay component for displaying a menu of links
 const MenuOverlay = ({ links }) => {
   return (
-    // List of links displayed vertically
+    // This creates a vertical list of links
     <ul className="flex flex-col py-4 items-center">
-      {/* Mapping through the array of links */}
+      {/* Loop through the array of links to create a list item for each one */}
       {links.map((link, index) => (
-        <li key={index}>
-          {/* Each link is rendered using the NavLink component */}
+        <li key={index}> {/* Each list item needs a unique key */}
+          {/* Render each link using the NavLink component */}
           <NavLink href={link.path} title={link.title} />
         </li>
       ))}
@@ -17,4 +19,5 @@ const MenuOverlay = ({ links }) => {
   );
 };
 
+// Export the MenuOverlay component so it can be used in other parts of the app
 export default MenuOverlay;
